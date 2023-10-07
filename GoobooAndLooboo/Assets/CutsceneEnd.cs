5b22131fd06eb45f7787ebf84b422ec853650789
bbt795 <bubby1031@hotmail.com>
2023-10-06 21:52:12 -0400

    Added CutsceneScene
    Added scene for cutscene, and script to move from cutscene to level 1 on video completion

---
+++ GoobooAndLooboo/Assets/CutsceneEnd.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CutsceneEnd : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public string newGameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        videoPlayer.loopPointReached += NextScene;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void NextScene(VideoPlayer videoPlayer){
+
+        SceneManager.LoadScene(newGameScene);
+
+    }
+}
