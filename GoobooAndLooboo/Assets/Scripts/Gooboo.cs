5da7ca5ef9a19a034859f5cebacef6d42cc67557
bbt795 <bubby1031@hotmail.com>
2023-10-07 23:50:52 -0400

    Fix Jump

--- GoobooAndLooboo/Assets/Scripts/Gooboo.cs
+++ GoobooAndLooboo/Assets/Scripts/Gooboo.cs
@@ -81,12 +81,14 @@ public class Gooboo : MonoBehaviour
             }
         }
 
-        //myRig.velocity = transform.forward * speed * lastDirection.y + new Vector3(0, myRig.velocity.y, 0);
-        myRig.velocity = new Vector2(lastDirection.x, lastDirection.y).normalized * speed;
+
+        //myRig.velocity = transform.forward * speed * new Vector2(0, myRig.velocity.y);
+        var velocity = new Vector2(lastDirection.x, 0f).normalized * speed;
+        myRig.velocity = new Vector2(velocity.x, myRig.velocity.y);
 
         if (lastJump)
         {
-            myRig.velocity += new Vector2(0, 8);
+            myRig.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
             //this is poor practice, it is better to have a parameter/class variable
             //you can change in the editor rather than a random number
             lastJump = false;
@@ -94,16 +96,13 @@ public class Gooboo : MonoBehaviour
         }
         else if (!canJump && myRig.velocity.y <= 0)
         {
-            RaycastHit check;
-            if (Physics.SphereCast(this.transform.position, .2f, this.transform.up * -1, out check))
+            RaycastHit2D check = Physics2D.CircleCast(this.transform.position, .2f, this.transform.up * -1);
+            if (check.distance < 0.2f)
             {
                 //out means that whatever variable is inputted next to it will be populated by the function it is in
-                if (check.distance < .6f)
-                {
                     canJump = true;
                     myAnim.SetInteger("DIR", 0);
 
-                }
             }
             //^^^ These two if statements are probably the safest way to perform jump checks
 
